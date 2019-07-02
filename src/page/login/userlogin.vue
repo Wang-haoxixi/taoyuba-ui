5be8166343880d7e53d528729bbbe9645997f6b8
“ <845081382@qq.com>
2019-07-02 08:38:33 +0800

    add

--- src/page/login/userlogin.vue
+++ src/page/login/userlogin.vue
@@ -21,7 +21,7 @@
       <div class="login-text">
         <el-checkbox v-model="form.isKeepLogin">保持登陆</el-checkbox>
         <div class="check-text">
-          <el-button type="text" @click.prevent="handleRetrieve">忘记密码?</el-button>
+          <!-- <el-button type="text" @click.prevent="handleRetrieve">忘记密码?</el-button> -->
           <el-button type="text" @click.prevent="handleRegister">立即注册</el-button>
         </div>
       </div>
