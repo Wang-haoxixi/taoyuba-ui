9f4ad3e1f58432e2ff3ed91d22b82b66d0f082c2
wh <1570058176@qq.com>
2022-01-13 09:13:24 +0800

    在线视频增加学习记录

---
+++ src/views/lessonVideo/page/learnedUer.vue
@@ -0,0 +1,171 @@
+<template>
+    <div>
+        <basic-container>
+            <page-header title="在线视频"></page-header>
+            <operation-container>
+                <template slot="left">
+                    <iep-button @click="refresh">刷新</iep-button>
+                </template>
+                <template slot="right">
+                    <el-input v-model="params.idCard" placeholder="身份证" size="small" clearable style="width:150px"></el-input>
+                    <el-select clearable size="small" v-model="params.type" placeholder="请选择类型" style="width:150px!important">
+                        <el-option
+                        label="船东"
+                        value="1">
+                        </el-option>
+                        <el-option
+                        label="职务船员"
+                        value="2">
+                        </el-option>
+                        <el-option
+                        label="游客"
+                        value="3">
+                        </el-option>
+                    </el-select>
+                    <el-input v-model="params.name" placeholder="姓名" size="small" clearable style="width:150px"></el-input>
+                    <el-button size="small" @click="search">搜索</el-button>
+                </template>
+            </operation-container>
+            <el-table
+            :data="userData"
+            :header-cell-style="headerCellStyle"
+            style="width: 100%">
+                <el-table-column
+                    prop="vedioName"
+                    label="视频标题">
+                </el-table-column>
+                <el-table-column
+                    label="视频图片">
+                    <template slot-scope="scope">
+                        <el-image :src='scope.row.videoImg' style="width:150px"></el-image>
+                    </template>
+                </el-table-column>
+                <el-table-column
+                    prop="videoDescript"
+                    label="视频简介">
+                </el-table-column>
+                <el-table-column
+                    prop="idcard"
+                    label="身份证">
+                </el-table-column>
+                <el-table-column
+                    prop="updateTime"
+                    label="最近一次学习时间">
+                </el-table-column>
+                <el-table-column
+                    label="学习记录">
+                    <template slot-scope="scope">
+                        <el-button size="small" @click="check(scope.row)">查看</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <el-pagination
+                style="text-align:center;margin-top:15px"
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="params.current"
+                :page-sizes="[10,20,30,40]"
+                :page-size="params.size"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="total">
+            </el-pagination>
+        </basic-container>
+    </div>
+</template>
+
+<script>
+    import { getUserInfoList } from '@/api/lessonVideo'
+    export default {
+        data (){
+            return{
+                params:{
+                    current:1,
+                    size:10,
+                    idCard:'',
+                    type:'',
+                    name:'',
+                },
+                total:0,
+                userData:[],
+            }
+        },
+        created (){
+            console.log(12313, this.formatSeconds(90))
+            this.getUserInfoList()
+        },
+        methods:{
+            initParams (){
+                this.params={
+                    current:1,
+                    size:10,
+                    idCard:'',
+                    type:'',
+                    name:'',
+                }
+            },
+            // 秒转换为时分秒
+            formatSeconds (value) {
+				var secondTime = parseInt(value) // 秒
+				var minuteTime = 0 // 分
+				var hourTime = 0 // 小时
+				if (secondTime >= 60) {
+					minuteTime = parseInt(secondTime / 60)
+					secondTime = parseInt(secondTime % 60)
+					if (minuteTime >= 60) {
+						hourTime = parseInt(minuteTime / 60)
+						minuteTime = parseInt(minuteTime % 60)
+					}
+				}
+				var result ='' +(parseInt(secondTime) < 10? '0' + parseInt(secondTime): parseInt(secondTime))
+
+				// if (minuteTime > 0) {
+					result ='' + (parseInt(minuteTime) < 10? '0' + parseInt(minuteTime) : parseInt(minuteTime)) + ':' + result
+				// }
+				// if (hourTime > 0) {
+					result ='' + (parseInt(hourTime) < 10 ? '0' + parseInt(hourTime): parseInt(hourTime)) +':' + result
+				// }
+				return result
+			},
+            headerCellStyle ({ rowIndex }){
+                if (rowIndex == 0) {
+                    return 'background:#F2F4F5;color:#000'
+                } else {
+                    return ''
+                }
+            },
+            getUserInfoList (){
+                getUserInfoList(this.params).then(({ data })=>{
+                    console.log('getUserInfoList..', data)
+                    this.total = data.data.total
+                    this.userData = data.data.records
+                })
+            },
+            handleSizeChange (val) {
+                this.params.size = val
+                this.getUserInfoList()
+            },
+            handleCurrentChange (val) {
+                this.params.current = val
+                this.getUserInfoList()
+            },
+            check (row){
+                console.log('check..', row)
+            },
+            search (){
+                console.log('search..')
+            },
+            refresh (){
+                this.initParams()
+            },
+        },
+        computed:{
+            id () {
+                return this.$route.params.id
+            },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
